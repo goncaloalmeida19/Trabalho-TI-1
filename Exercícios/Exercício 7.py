89a113be6e0b19adfa8cee551c16ef605bfb91ea
goncaloalmeida17 <gamertota4@gmail.com>
2021-11-08 23:57:09 +0000

    Adicionei medquadbits

--- Exercícios/Exercício 7.py
+++ Exercícios/Exercício 7.py
@@ -6,9 +6,17 @@ def med_bits(pr, P):
     media = sum((np.array(lengths) * np.array(pr)))
     return media
 
+# Função que calcula a média de bits por símbolo com os tamanhos ao quadrado
+# (para depois usar na fórmula da variância)
+def med_quad_bits(pr, P):
+    codec = HuffmanCodec.from_frequencies(P)
+    symbols, lengths = codec.get_code_len()
+
+    media = sum((np.array(lengths)**2 * np.array(pr)))
+    return media
 
 def agrupa(f):
     list = []
     for i in range(int(len(f) / 2 - 1)):
         list.append((f[i * 2], f[i * 2 + 1]))
-    return list
+return list
